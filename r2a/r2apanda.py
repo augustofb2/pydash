97640d2c5f0556174ae607c4c73879d6d87539d9
Fernando <nandoo44@hotmail.com>
2020-12-07 16:03:26 -0300

    Changed some variables and corrected smooth calcule

--- r2a/r2apanda.py
+++ r2a/r2apanda.py
@@ -12,17 +12,16 @@ class R2APanda(IR2A):
 
     def __init__(self, id):
         IR2A.__init__(self, id)
-        self.calculated = []        # calculated target throughputs (x^)
-        self.smoothed = []          # smoothed troughputs (y^)
-        self.measured = []          # real measured throughputs (x~)
-        self.quantized = []         # quantized troughputs (r)
+        self.calculated = 0        # calculated target throughputs (x^)
+        self.smoothed = 0          # smoothed troughputs (y^)
+        self.measured = 0          # real measured throughputs (x~)
+        self.quantized = 0         # quantized troughputs (r)
         self.video_seg_time = 1     # each segment time has 1s of duration (tau)
-        self.actual_inter_request_time = [] # (T)
+        self.actual_inter_request_time = 0 # (T)
         self.schedule = 0           # time between segments downloads (T^)
         self.download_time = 0      # segment download duration (T~)
         self.request_time = 0       
         self.qi = []                # video quality index
- 
 
 
     def handle_xml_request(self, msg):
@@ -37,21 +36,20 @@ class R2APanda(IR2A):
         download_time = time.perf_counter() - self.request_time
 
         # first iteraction
-        if len(self.measured) == 0:
-            self.measured.append(msg.get_bit_length() / download_time)
+        if self.measured == 0:
+            self.measured = msg.get_bit_length() / download_time
 
         self.send_up(msg)
-
 
 
     def handle_segment_size_request(self, msg):
         self.request_time = time.perf_counter()
 
-        self.calculated.append(self.target_throughput())
-        self.smoothed.append(self.smoothed_throughput())
-        self.quantized.append(self.select_qi())
+        self.calculated = self.target_throughput()
+        self.smoothed = self.smoothed_throughput()
+        self.quantized = self.select_qi()
 
-        msg.add_quality_id(self.quantized[-1])
+        msg.add_quality_id(self.quantized)
 
         self.send_down(msg)
 
@@ -60,8 +58,8 @@ class R2APanda(IR2A):
         self.schedule_handler()
         download_time = time.perf_counter() - self.request_time
 
-        self.actual_inter_request_time.append(max(self.schedule, download_time))
-        self.measured.append(msg.get_bit_length() / download_time)
+        self.actual_inter_request_time = max(self.schedule, download_time)
+        self.measured = msg.get_bit_length() / download_time
 
         self.send_up(msg)
 
@@ -73,25 +71,29 @@ class R2APanda(IR2A):
 
 
     def target_throughput(self):
-        x = self.measured[0]    # first interaction
         k = 0.14                # probing convergence rate
         w = 0.3 * 1048576       # probing additive increace megabitrate
 
-        if len(self.measured) > 1:
-            x = abs((w - max((0, self.calculated[-1] - self.measured[-1] + w)))
-                * k * self.actual_inter_request_time[-1] + self.calculated[-1])
+        if self.calculated != 0:
+            x = (k * (w - max(0, self.calculated - self.measured + w)) 
+                * self.actual_inter_request_time + self.calculated)
+            return x
+
+        x = self.measured       # first interaction
         
         return x
 
 
     def smoothed_throughput(self):
-        y = self.measured[0]    # first interaction
         alpha = 0.2             # smoothing convergence rate
 
-        if len(self.measured) > 1:
-            y = abs(-alpha * (self.smoothed[-1] - self.calculated[-1])
-                    / self.actual_inter_request_time[-1] + self.smoothed[-1])
+        if self.smoothed != 0:
+            y = abs(-alpha * (self.smoothed - self.calculated) 
+                    * self.actual_inter_request_time + self.smoothed)
+            return y
         
+        y = self.measured    # first interaction
+
         return y
 
 
@@ -99,8 +101,8 @@ class R2APanda(IR2A):
         E = 0.15                # multiplicative safety margin
         r_up = self.qi[0]       # upshift threshold for dead-zone in quantization
         r_down = self.qi[0]     # dowmshift threshold for dead-zone in quantization
-        initial_r_up = self.smoothed[-1] * (1 - E)
-        initial_r_down = self.smoothed[-1]
+        initial_r_up = self.smoothed * (1 - E)
+        initial_r_down = self.smoothed
 
         for quaity in self.qi:
             if initial_r_up > quaity:
@@ -109,12 +111,12 @@ class R2APanda(IR2A):
                 r_down = quaity
         
         # updates dead-zone quantizier
-        if len(self.quantized) == 0:
+        if self.quantized == 0:
             return r_down
-        elif self.quantized[-1] < r_up:
+        elif self.quantized < r_up:
             return r_up
-        elif r_up <= self.quantized[-1] < r_down:
-            return self.quantized[-1]
+        elif r_up <= self.quantized < r_down:
+            return self.quantized
         
         return r_down
 
@@ -124,6 +126,5 @@ class R2APanda(IR2A):
         beta = 0.2          # client buffer convergence duration
         B = self.whiteboard.get_amount_video_to_play()  # client buffer duration
 
-        self.schedule = self.quantized[-1] * self.video_seg_time / self.smoothed[-1] \
-                        + beta * (B - B_min)
+        self.schedule = self.quantized * self.video_seg_time / self.smoothed + beta * (B - B_min)
         
